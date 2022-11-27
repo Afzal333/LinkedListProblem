fe870eb8310f979aa1bff38de380fb7e174b3e44
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-27 23:33:09 +0530

    [Afzal] Basic structure

---
+++ LinkedListProblem/LinkedListProblem/LinkedList.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+
+namespace LinkedListProblem
+{
+    public class LinkedList<T>
+    {
+        internal Node<T> head;
+        internal void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} inserted into linked list",node.data);
+        }
+        internal void AddInReverseOrder(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node<T> temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        internal void Delete()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linkedlist is empty");
+            }
+            if(head.next == null)
+            {
+                head = null;
+            }
+            Node<T> NewNode = head;
+            while (NewNode.next.next != null)
+            {
+                NewNode = NewNode.next;
+            }
+            NewNode.next = null;
+        }
+        
+        internal void Display()
+        {
+            Node<T> temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + "");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedListProblem/LinkedListProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace LinkedListProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LinkedList");
+            //LinkedList list = new LinkedList();
+            //list.Add(56);
+            //list.Add(30);
+            //list.Add(70);        
+            //list.Size();
+        }
+    }
+}
